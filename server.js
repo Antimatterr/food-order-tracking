0ef6981624d30a38daf7a4ab31ab85a8916c8bc1
sidhant singh <singhsidmail23@gmail.com>
2022-08-26 13:29:57 +0530

    login and register page added

--- server.js
+++ server.js
@@ -9,14 +9,27 @@ const path = require('path');
 //assets
 app.use(express.static('public'))
 
-app.get('/', (req, res) => {
-  res.render('home')
-})
 
 //set tempplate engine
 app.use(expressLayout);
 app.set('views', path.join(__dirname, '/resources/views'))
 app.set('view engine', 'ejs')
+
+app.get('/', (req, res) => {
+  res.render('home')
+})
+
+app.get('/cart', (req, res, next) => {
+  res.render('customers/cart')
+})
+
+app.get('/login', (req, res, next) => {
+  res.render('auth/login')
+})
+
+app.get('/register', (req, res, next) => {
+  res.render('auth/register')
+})
 
 
 app.listen(PORT, () => {
