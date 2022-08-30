a02ff5ec6bd9520ad35805da03a3418be7b858b1
sidhant singh <singhsidmail23@gmail.com>
2022-08-30 23:52:42 +0530

    add to cart

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,31 @@
-console.log("hello")
+import axios from 'axios';
+import Noty from 'noty';
+
+let addToCart = document.querySelectorAll('.add-to-cart')
+let cartCounter = document.getElementById('cartCounter')
+let negativeCount = document.querySelectorAll('.negativeCount')
+let plusCount = document.querySelectorAll('.plusCount');
+
+function updateCart(pizza) {
+  axios.post('/update-cart', pizza).then(res => {
+    console.log(res)
+    cartCounter.innerText = res.data.totalQty;
+    new Noty({
+      text: "Item added to cart",
+      type: 'success',
+      timeout: 1000,
+    }).show()
+  })
+    .catch(err => {
+      new Noty({
+        text: "Something went wrong",
+        type: 'error',
+        timeout: 1000,
+      }).show()
+    })
+}
+
+
+
+
+
--- server.js
+++ server.js
@@ -1,35 +1,64 @@
 const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3000;
+const mongoose = require('mongoose');
 const ejs = require('ejs');
 const expressLayout = require('express-ejs-layouts');
 const path = require('path');
+const initRoutes = require('./routes/web');
+require('dotenv').config();
+const session = require('express-session');
+const flash = require('express-flash');
+const MongoStore = require('connect-mongo');
 
+
+// database connection
+mongoose.connect(process.env.DB_URL, { useNewUrlParser: true, useUnifiedTopology: false });
+const db = mongoose.connection;
+
+db.on('error', console.error.bind(console, 'console error:'));
+db.once('open', () => {
+  console.log("db connected")
+})
+
+
+// session store
+
+// session configuratiom
+app.use(session({
+  secret: process.env.COOKIE_SECRET,
+  resave: false,
+  store: MongoStore.create({
+    mongoUrl: process.env.DB_URL,
+    collectionName: 'sessions'
+  }),
+  saveUninitialized: false,
+  cookie: { maxAge: 1000 * 60 * 60 * 24 } // 24 hours
+}))
+
+
+app.use(flash())
 
 //assets
 app.use(express.static('public'))
 
+// json
+app.use(express.json());
+
+// gloal middleware
+
+//if we given next in middleware we have to call that callback other wise requuest will hang
+app.use((req, res, next) => {
+  res.locals.session = req.session;
+  next();
+})
 
 //set tempplate engine
 app.use(expressLayout);
 app.set('views', path.join(__dirname, '/resources/views'))
 app.set('view engine', 'ejs')
 
-app.get('/', (req, res) => {
-  res.render('home')
-})
-
-app.get('/cart', (req, res, next) => {
-  res.render('customers/cart')
-})
-
-app.get('/login', (req, res, next) => {
-  res.render('auth/login')
-})
-
-app.get('/register', (req, res, next) => {
-  res.render('auth/register')
-})
+initRoutes(app);
 
 
 app.listen(PORT, () => {
